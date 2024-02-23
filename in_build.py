6e4fcf11c8442e6a0774c89b4e2f6496ccf2c023
Dipa Chiniya Parasaram <157943824+Dipa-chiniyaa@users.noreply.github.com>
2024-02-23 08:40:34 +0530

    Add files via upload

---
+++ in_build.py
@@ -0,0 +1,10 @@
+# import math
+# print(dir(math))
+
+# only for square root module
+
+from math import sqrt
+
+n = input("Enter a number : ")
+n = int(n)
+print(sqrt(n))
---
+++ user_define.py
@@ -0,0 +1,16 @@
+# syntax 
+# def function_name(parameters);
+#     # code
+
+def print_sum(first,second):
+    print(first + second)
+    
+    
+# function call
+n1 = input("Enter first number : ")
+n1=int(n1)
+
+n2 = input("Enter the second number : ")
+n2 = int(n2)
+
+print_sum(n1,n2)
